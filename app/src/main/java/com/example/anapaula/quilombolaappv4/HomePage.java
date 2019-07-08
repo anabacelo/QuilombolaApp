fd7557f6e8a016e96659545ecba73d7b805c85f2
thiagoroly <sirdurandall@gmail.com>
2019-07-08 17:03:05 -0300

    Criando a Homepage em HTML, stash apply

--- app/src/main/java/com/example/anapaula/quilombolaappv4/HomePage.java
+++ app/src/main/java/com/example/anapaula/quilombolaappv4/HomePage.java
@@ -6,11 +6,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HomePage extends Fragment {
+public class HomePage extends Fragment implements IOnBackPressed{
+
+    private WebView mWebView;
 
     public HomePage() {
         // Required empty public constructor
@@ -20,6 +24,31 @@ public class HomePage extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_page, container, false);
+        View view = inflater.inflate(R.layout.fragment_home_page_web, container, false);
+
+        mWebView = (WebView) view.findViewById(R.id.webViewHomePage);
+        // Enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        // Stop local links and redirects from opening in browser instead of WebView
+        mWebView.setWebViewClient(new WebViewClientOverride());
+
+        // Load remote site
+        //mWebView.loadUrl("http://app-quilombola.epizy.com/noticias.html");
+        //mWebView.loadUrl("http://www.html5rocks.com/");
+        mWebView.loadUrl("file:///android_asset/www/home.html");
+        return view;
+    }
+
+    @Override
+    public boolean onBackPressed() {
+        if (mWebView.canGoBack()) {
+            mWebView.goBack();
+            //action not popBackStack
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- app/src/main/java/com/example/anapaula/quilombolaappv4/Nav1.java
+++ app/src/main/java/com/example/anapaula/quilombolaappv4/Nav1.java
@@ -7,7 +7,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
 /**
  * A simple {@link Fragment} subclass.
  */
