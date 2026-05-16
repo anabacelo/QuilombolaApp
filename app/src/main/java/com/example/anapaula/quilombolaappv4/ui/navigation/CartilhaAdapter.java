89e3b6656424d4929291a06112b9781de4ce7433
JVFreitas03 <joao.freitas007@edu.pucrs.br>
2026-05-16 13:43:31 -0300

    Update CartilhaAdapter.java

--- app/src/main/java/com/example/anapaula/quilombolaappv4/ui/navigation/CartilhaAdapter.java
+++ app/src/main/java/com/example/anapaula/quilombolaappv4/ui/navigation/CartilhaAdapter.java
@@ -15,41 +15,50 @@ import java.util.List;
 
 public class CartilhaAdapter extends RecyclerView.Adapter<CartilhaAdapter.ViewHolder> {
 
-    List<String> titulos;
-    List<String> textos;
+    private final List<String> titulos;
+    private final List<String> textos;
+    private final List<Integer> imagens;
 
-    public CartilhaAdapter(List<String> titulos, List<String> textos) {
+    public CartilhaAdapter(List<String> titulos,
+                           List<String> textos,
+                           List<Integer> imagens) {
+
         this.titulos = titulos;
         this.textos = textos;
+        this.imagens = imagens;
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        TextView titulo, texto;
+
+        TextView titulo;
+        TextView texto;
         ImageView imagem;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            titulo = itemView.findViewById(R.id.tituloPagina);
-            texto = itemView.findViewById(R.id.textoPagina);
-            imagem = itemView.findViewById(R.id.imagemPagina);
+
+            titulo = itemView.findViewById(R.id.tituloCartilha);
+            texto = itemView.findViewById(R.id.textoCartilha);
+            imagem = itemView.findViewById(R.id.imagemCartilha);
         }
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
         View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.page_cartilha, parent, false);
+                .inflate(R.layout.page_cartilha_sb, parent, false);
+
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
         holder.titulo.setText(titulos.get(position));
         holder.texto.setText(textos.get(position));
-
-        // imagem opcional por enquanto
-        holder.imagem.setImageResource(R.mipmap.quilombola_icon);
+        holder.imagem.setImageResource(imagens.get(position));
     }
 
     @Override
